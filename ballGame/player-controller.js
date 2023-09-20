82b5050431ef7a7fdcb72692ab04c9617e7d18c3
Jordyn <jordynjr2004@gmail.com>
2023-09-20 09:43:51 -0600

    redid ball game

--- ballGame/player-controller.js
+++ ballGame/player-controller.js
@@ -1,19 +1,27 @@
 AFRAME.registerComponent("player-controller", {
-  init(){
-    document.addEventListener("click", () => {
-        const newBall = this.el.sceneEl.components.pool_ball.requestEntity();
+    init() {
+      document.addEventListener("click", () => {
+        // get reference of the pool of objects from the aframe scene
+        const newBall = this.el.sceneEl.components.pool__ball.requestEntity();
+        // activate the entity we got
         newBall.play();
-        const implulseV = new CANNON.Vec3();
+        // create a vector to store the impulse direction and value
+        const impulseV = new CANNON.Vec3();
+        // take the forward direction of the camera and assign it's values to our impulse vector
         this.el.sceneEl.camera.getWorldDirection(impulseV);
+        
+        // Move the spawned ball to
         newBall.body.position.copy(this.el.object3D.position);
-        newBall.body.position.x += 0.3;
+        newBall.body.position.x -= 0.3;
         newBall.body.position.y -= 0.2;
         
         newBall.body.applyImpulse(
-            impulseV.scale(90), new CANNON.Vec3().copy(newBall.body.position)
+          impulseV.scale(90),
+          new CANNON.Vec3().copy(newBall.body.position)
         );
-      
-        console.log(newBallewBall.body);
-    });
-  }
-});
+        
+        console.log(newBall.body);
+        
+      });
+    }
+  });
