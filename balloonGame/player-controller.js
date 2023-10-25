362d3233c89b3ee0fa13d9a5c014e48caf8656b0
Jordyn <jordynjr2004@gmail.com>
2023-10-25 13:13:41 -0600

    vr game

--- balloonGame/player-controller.js
+++ balloonGame/player-controller.js
@@ -8,13 +8,26 @@ AFRAME.registerComponent("teleporter", {
       const getHit = ray.getIntersection(
         document.querySelector(".teleport-target")
       );
-      console.log(getHit);
       if (getHit) {
         console.log(getHit);
         this.el.object3D.position.x = getHit.point.x;
         this.els.object3D.position.z = getHit.point.z;
       } else {
+        console.log(score);
         if (balls <= 0) return;
+  
+        else if(score >= 5){
+          document.querySelector("#score").setAttribute('text','value: YOU WON AN OCTO!!!!');
+          document.querySelector("#score").setAttribute('scale', '5 5 5');
+          document.querySelector("#score").setAttribute('position', {x: -10, y: 5, z: -5});
+          let prize = document.querySelector('#prize');
+          prize.setAttribute('scale', '1 1 1');
+          prize.setAttribute('position', {x: -7, y: 1.5, z: -5});
+          document
+          .querySelector("#balls")
+          .setAttribute("text", "value: Game Over :(");
+          return;
+        }
         // get reference of the pool of objects from the aframe scene
         const newBall = this.el.sceneEl.components.pool__dart.requestEntity();
         // activate the entity we got
@@ -39,7 +52,7 @@ AFRAME.registerComponent("teleporter", {
         balls = balls - 1;
         document
           .querySelector("#balls")
-          .setAttribute("text", "value: Balls left = " + balls);
+          .setAttribute("text", "value: Darts left = " + balls);
         if (balls <= 0) {
           document
             .querySelector("#balls")
